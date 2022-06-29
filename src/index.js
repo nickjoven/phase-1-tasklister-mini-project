31362965504380452b3c3e2ef1d24bef78eaa4da
Nicholas <nicholasjoven@gmail.com>
2022-06-29 14:24:02 -0400

    added change listener

--- src/index.js
+++ src/index.js
@@ -10,15 +10,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const createListItem = () => {
       const userTextBox = document.querySelector("#new-task-description")
       const userText = userTextBox.value
-      console.log(userText)
-      let li = document.createElement('li')
+      const li = document.createElement('li')
       li.innerText = userText
+      li.setAttribute("class", "highPrio")
       const tasksList = document.querySelector("#tasks")
       tasksList.append(li)
+      // adding dropdown
+      const dropdown = document.createElement('select')
+      dropdown.setAttribute("class", "dropdownMenu")
+      li.append(dropdown)
+      const highPriority = document.createElement('option')
+      highPriority.value = 'high'
+      highPriority.innerText = 'High'
+      const mediumPriority = document.createElement('option')
+      mediumPriority.value = 'medium'
+      mediumPriority.innerText = 'Medium'
+      const lowPriority = document.createElement('option')
+      lowPriority.value = 'low'
+      lowPriority.innerText = 'Low'
+      dropdown.append(highPriority, mediumPriority, lowPriority)
     }
+ 
     createListItem();
-  })
+  
+    const changeMenu = document.querySelectorAll('.dropdownMenu');
+
+    changeMenu.addEventListener('change', (event) => {
+      console.log("Menu changed")})
+    changeMenu.parentElement.className = ""
+
+
+    })
 })
-
-
-
