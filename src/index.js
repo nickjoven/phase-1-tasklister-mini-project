ef3d2349e829256e6471abc09583e10010d52cac
Nicholas <nicholasjoven@gmail.com>
2022-06-29 15:09:20 -0400

    updated select  options

--- src/index.js
+++ src/index.js
@@ -7,27 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
   formSubmit.addEventListener('submit', (event) => {
     // alert("I heard a submit event")
     event.preventDefault();
-
+    
+    const userTextBox = document.querySelector("#new-task-description")
+    const userText = userTextBox.value
 
     const addToArray = () => {
-      const userTextBox = document.querySelector("#new-task-description")
-      const userText = userTextBox.value
       taskArray.push(userText)
     }
 
     addToArray();
 
-    const createListItem = () => {
-      const userTextBox = document.querySelector("#new-task-description")
-      const userText = userTextBox.value
+    const createListFromArray = () => {
       const li = document.createElement('li')
       li.innerText = userText
-      li.setAttribute("class", "highPrio")
       const tasksList = document.querySelector("#tasks")
       tasksList.append(li)
-      // adding dropdown
       const dropdown = document.createElement('select')
-      dropdown.setAttribute("class", "dropdownMenu")
+      dropdown.setAttribute("class", `dropdownMenu${taskArray.length}`)
       li.append(dropdown)
       const highPriority = document.createElement('option')
       highPriority.value = 'high'
@@ -39,16 +35,44 @@ document.addEventListener("DOMContentLoaded", () => {
       lowPriority.value = 'low'
       lowPriority.innerText = 'Low'
       dropdown.append(highPriority, mediumPriority, lowPriority)
-    }
+     }
+
+     createListFromArray();
+
+
+
+    // const createListItem = () => {
+    //   const userTextBox = document.querySelector("#new-task-description")
+    //   const userText = userTextBox.value
+      // const li = document.createElement('li')
+      // li.innerText = userText
+    //   li.setAttribute("class", "highPrio")
+    //   const tasksList = document.querySelector("#tasks")
+    //   tasksList.append(li)
+    //   // adding dropdown
+      // const dropdown = document.createElement('select')
+      // dropdown.setAttribute("class", "dropdownMenu")
+      // li.append(dropdown)
+      // const highPriority = document.createElement('option')
+      // highPriority.value = 'high'
+      // highPriority.innerText = 'High'
+      // const mediumPriority = document.createElement('option')
+      // mediumPriority.value = 'medium'
+      // mediumPriority.innerText = 'Medium'
+      // const lowPriority = document.createElement('option')
+      // lowPriority.value = 'low'
+      // lowPriority.innerText = 'Low'
+      // dropdown.append(highPriority, mediumPriority, lowPriority)
+    // }
  
-    createListItem();
+//     createListItem();
   
-    const changeMenu = document.querySelector('.dropdownMenu');
+//     const changeMenu = document.querySelector('.dropdownMenu');
 
-    changeMenu.addEventListener('change', (event) => {
-      console.log("Menu changed")})
-    changeMenu.parentElement.className = ""
+//     changeMenu.addEventListener('change', (event) => {
+//       console.log("Menu changed")})
+//     changeMenu.parentElement.className = ""
 
 
-    })
+})
 })
