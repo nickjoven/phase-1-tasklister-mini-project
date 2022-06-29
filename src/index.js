d8b7630b387ed1df83762920bd296e4c7b8cd12a
Nicholas <nicholasjoven@gmail.com>
2022-06-29 11:35:40 -0400

    initial completion of deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  
+  
+  const formSubmit = document.querySelector("#create-task-form")
+  formSubmit.addEventListener('submit', (event) => {
+    // alert("I heard a submit event")
+    event.preventDefault();
+
+    const createListItem = () => {
+      const userTextBox = document.querySelector("#new-task-description")
+      const userText = userTextBox.value
+      console.log(userText)
+      let li = document.createElement('li')
+      li.innerText = userText
+      const tasksList = document.querySelector("#tasks")
+      tasksList.append(li)
+    }
+    createListItem();
+  })
+})
+
+
+
