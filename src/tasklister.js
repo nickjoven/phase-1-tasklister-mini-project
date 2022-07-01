3d00b3cc95aabb480728c96ababe5824caf62ddd
Nicholas <nicholasjoven@gmail.com>
2022-07-01 05:51:12 -0400

    store to array funciton on submit added

--- src/tasklister.js
+++ src/tasklister.js
@@ -1,14 +1,25 @@
+let taskArray = []
 document.addEventListener("DOMContentLoaded", () => {
- // create empty array
-  let taskArray = []
+  // create empty array
 
-  const inputText = document.getElementById('new-task-description')
+  // variable declarations for HTML elements
+  const inputTextBox = document.getElementById('new-task-description')
+  const inputDateBox = document.getElementById('new-task-due-date')
+  const inputPrioBox = document.getElementById('new-task-priority')
+  const form = document.getElementById('create-task-form')
 
-  const inputDate = document.getElementById('new-task-due-date')
+  const clearForm = () => {
+    inputTextBox.value = ""
+    inputDateBox.value = ""
+    inputPrioBox.value = ""
+  }
 
-  const inputPrio = document.getElementById('new-task-priority')
+  let inputText = inputTextBox.value
+  let inputDate = inputDateBox.value
+  let inputPrio = inputPrioBox.value
 
 
+// convert priority selection to precdence value for sorting
   const prioConvert = (inputPrio) => {
     let precedence = 3
     if (inputPrio == "") {
@@ -24,15 +35,26 @@ document.addEventListener("DOMContentLoaded", () => {
       return precedence
     }
   }
-    
-  const inputPrecdence = prioConvert(inputPrio)
 
-  const addInput = () => {
-    const taskObj = {
+  // create a function that grabs the values from the HTML elements and stores them
+  // put values in object, push object to array
+
+  const inputToArray = () => {
+    const obj = {
+      id: taskArray.length + 1,
       text: inputText,
       date: inputDate,
-      priority: inputPrio,
-      precedence: inputPrecdence
+      prio: inputPrio,
+      precedence: prioConvert(inputPrio),
     }
+    taskArray.push(obj)
   }
+
+  // execute on submit
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    inputToArray(); // store values
+    clearForm(); // clear values
+    }
+  )    
 })
