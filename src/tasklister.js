5f90a8c4e1f55c1e10263b1c28b2a2e498fdf287
Nicholas <nicholasjoven@gmail.com>
2022-07-01 05:03:41 -0400

    set up tasklister.js skeleton

--- src/tasklister.js
+++ src/tasklister.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
- 
+ // create empty array
+  let taskArray = []
+
+  const inputText = document.getElementById('new-task-description')
+
+  const inputDate = document.getElementById('new-task-due-date')
+
+  const inputPrio = document.getElementById('new-task-priority')
+
+
+  const prioConvert = (inputPrio) => {
+    let precedence = 3
+    if (inputPrio == "") {
+      return precedence
+    } else if (inputPrio == "High") {
+      precedence = 0
+      return precedence
+    } else if (inputPrio == "Medium") {
+      precedence = 1
+      return precedence
+    } else if (inputPrio == "Low" ) {
+      precedence = 2
+      return precedence
+    }
+  }
+    
+  const inputPrecdence = prioConvert(inputPrio)
+
+  const addInput = () => {
+    const taskObj = {
+      text: inputText,
+      date: inputDate,
+      priority: inputPrio,
+      precedence: inputPrecdence
+    }
+  }
 })
